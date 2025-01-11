9351f8b5b05476227640cde48b1d5f9d31676262
1lsang <ilsangpark.98@gmail.com>
2025-01-12 01:52:54 +0900

    :sparkles: Add content collection

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+// glob 로더 가져오기
+import { glob } from 'astro/loaders';
+// `astro:content`에서 유틸리티 가져오기
+import { z, defineCollection } from 'astro:content';
+
+// 각 컬렉션에 대한 `loader` 및 `schema` 정의
+const content = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/contents' }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+  }),
+});
+
+// 단일 `collections` 객체를 내보내 컬렉션을 등록하세요
+export const collections = { content };
