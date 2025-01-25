61544d9ec6aae857e36aacf074539a3428eed08f
1lsang <ilsangpark.98@gmail.com>
2025-01-25 12:36:46 +0900

    :arrow_up: Update tailwind 3.4.17 → 4.0.0 (major)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false }), mdx()],
+  integrations: [react(), mdx({})],
+  vite: {
+    plugins: [tailwindcss()],
+  }
 });
