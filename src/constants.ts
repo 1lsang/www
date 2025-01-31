997a4df5f34d51f8a34fdaf5c144e3ffaabed741
1lsang <ilsangpark.98@gmail.com>
2025-01-31 11:04:50 +0900

    :sparkles: Add constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const NAVBAR_URL = [
+  { label: 'ABOUT', href: '/' },
+  { label: 'POSTS', href: '/1' },
+  // { label: 'PROJECT', href: '/project' },
+  // { label: 'PLAYGROUND', href: '/playground' },
+];
