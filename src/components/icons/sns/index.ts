d2b2725b22f6f0b9c0324f3b059251eb15fb4638
1lsang <ilsangpark.98@gmail.com>
2025-01-31 10:39:52 +0900

    :sparkles: Add sns icons.
    - Email, Github, LinkedIn, X

---
+++ src/components/icons/sns/index.ts
@@ -0,0 +1,4 @@
+export { default as EmailIcon } from './email.icon.astro';
+export { default as GithubIcon } from './github.icon.astro';
+export { default as LinkedInIcon } from './linked-in.icon.astro';
+export { default as XIcon } from './x.icon.astro';
