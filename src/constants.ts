cd039ab7c7c9f1ce45d378691fd51376ebc75966
1lsang <ilsangpark.98@gmail.com>
2025-01-31 12:25:27 +0900

    :sparkles: Add footer component

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,8 @@ export const NAVBAR_URL = [
   // { label: 'PROJECT', href: '/project' },
   // { label: 'PLAYGROUND', href: '/playground' },
 ];
+
+export const EMAIL = 'ilsangpark.98@gmail.com';
+export const GITHUB_URL = 'https://github.com/1lsang';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/ilsang-park-77b690257/';
+export const X_URL = 'https://www.x.com/';
